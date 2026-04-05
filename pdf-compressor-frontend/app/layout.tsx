c25f643ad9324fd25b56abbc3602b67ee7728e5c
Harsh2227kumar <harsh2227kumar@gmail.com>
2026-04-05 11:56:15 +0530

    Added Vercel Web Annalytics

--- pdf-compressor-frontend/app/layout.tsx
+++ pdf-compressor-frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import ClientBackground from "../components/ClientBackground";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "File Compressor",
@@ -13,14 +14,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <div className="app-container">
-          {/* This is now a safe Client Component */}
           <ClientBackground />
-          
           <Navbar />
-          <div className="content-wrap">
-            {children}
-          </div>
+          <div className="content-wrap">{children}</div>
         </div>
+        <Analytics />
       </body>
     </html>
   );
